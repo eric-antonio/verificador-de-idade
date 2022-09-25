ddcb95b2dc8d58ce2c7d9bde63e59bb9c5985110
Éric Ivan João António <ericantoniodev@gmail.com>
2022-09-25 19:28:19 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function verificar(){
+    var data =  new Date()
+    var ano = data.getFullYear()
+    var res = window.document.getElementById('res')
+    var txtano = window.document.getElementById('txtano')
+}
