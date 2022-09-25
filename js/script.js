f6eb64d9510fbe88f84c95c1ade1cbcd062d0d94
Éric Ivan João António <ericantoniodev@gmail.com>
2022-09-25 19:37:48 +0200

    Bug no primeiro if

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 function verificar(){
-    var data =  new Date()
+    var data = new Date()
     var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
     var res = window.document.getElementById('res')
-    var txtano = window.document.getElementById('txtano')
+
+    if(fano.value.length == 0 || Number(fano.value) > ano){
+        window.alert('Verifique o ano Novamente!')
+    }
 }
